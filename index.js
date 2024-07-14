6655f7ba03eec3e53057b9e8068734ff952a9a73
Heia Sarange Herrera <heiasarangeh@gmail.com>
2024-07-15 03:35:39 +0800

    Initialize project [v0.1.0]

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+
+app.get("/", (req, res) => {
+    res.status(200)
+    .json({
+        message: "ok"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
